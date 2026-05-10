e353d082406c9a8bdf7cb0a02f138eb255f7cbd9
Arnaldo Ferraz Leal Júnior <111538975+ArnaldoFerraz@users.noreply.github.com>
2026-05-10 19:58:56 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-alert("JavaScript carregado!");
-
 const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
+const shareBtn = document.getElementById("shareBtn");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", addTask);
 
-    if (input.value === "") {
+input.addEventListener("keypress", (e) => {
+
+    if (e.key === "Enter") {
+        addTask();
+    }
+
+});
+
+function addTask(){
+
+    const taskText = input.value.trim();
+
+    if(taskText === ""){
         return;
     }
 
     const li = document.createElement("li");
 
     li.innerHTML = `
-        <span>${input.value}</span>
+        <div class="task-content">
+            <span class="check">✓</span>
+            <span class="task-text">${taskText}</span>
+        </div>
+
         <button class="delete-btn">
             X
         </button>
@@ -23,12 +38,42 @@ button.addEventListener("click", () => {
 
     input.value = "";
 
-    li.querySelector("span").addEventListener("click", () => {
+    li.querySelector(".task-content").addEventListener("click", () => {
+
         li.classList.toggle("completed");
+
     });
 
     li.querySelector(".delete-btn").addEventListener("click", () => {
+
         li.remove();
+
     });
+}
+
+shareBtn.addEventListener("click", async () => {
+
+    let tasks = [];
+
+    document.querySelectorAll(".task-text").forEach(task => {
+
+        tasks.push("- " + task.textContent);
+
+    });
+
+    const text = tasks.join("\n");
+
+    if(navigator.share){
+
+        await navigator.share({
+            title: "Minha Checklist",
+            text: text
+        });
+
+    } else {
+
+        alert(text);
+
+    }
 
 });
